bd86af161e7dc36a3568815c4b94ee88443e79bb
Artur Tuyama <arturgtuyama@gmaill.com>
2020-09-24 21:00:56 -0300

    Primeiro commit

---
+++ MEDIA 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int numero, horas;
+    float valor, salario;
+    scanf("%d %d %f", &numero, &horas, &valor);
+    salario = horas * valor;
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", numero, salario);
+
+    return 0;
+}
